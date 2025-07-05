d1953e1fd19a4de4c2828692b5622bffa9680c69
Tirthankar Ghosh <tirthankarghosh4@gmail.com>
2025-07-05 21:40:07 +0530

    Add files via upload

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
+//Fetch the schema
 const User = require("../models/Userbymail");
+//Get the validator
 const { body, validationResult } = require("express-validator");
+//Get bcrypt
+const bcrypt = require("bcryptjs");
+//Get webtoken
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 //Create a user using: POST "/api/auth/createUser"
 router.post(
@@ -11,13 +18,13 @@ router.post(
     body("email", "Enter a valid email").isEmail(),
     body("password", "Password must be 8 characters long").isLength({ min: 8 }),
   ],
-  //check if validation is fine else send the error for the validation
+  //Check if validation is fine else send the error for the validation
   async (req, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
       return res.status(400).json({ errors: result.array() });
     }
-    //check whether the user with same email exists already
+    //Check whether the user with same email exists already
     try {
       let user = await User.findOne({ email: req.body.email });
       if (user) {
@@ -25,12 +32,21 @@ router.post(
           .status(400)
           .json({ error: "Sorry user with this email already exist" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password,salt);
+      //Create new user with given details from json(req.body)
       user = await User.create({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data = {
+        user:{
+            id: user.id
+        }
+      }
+      const authToken=jwt.sign('data',process.env.JWT_SECRET)
+      res.json({authToken});
     } catch (err) {
       console.error(err);
       res.status(500).send("Som error occured");
