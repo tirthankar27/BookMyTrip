53c4d4738f1b9694b4bbc6c77739b5fcd15832f1
tirthankar27 <tirthankarghosh4@gmail.com>
2026-06-24 00:15:09 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 // Simple CORS configuration
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://book-my-trip-9wmq.vercel.app"]
   // credentials: true
 }));
 
